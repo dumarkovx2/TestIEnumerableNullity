f8aa342d6cbd1f822a93b24f925d0636aecb4c20
David Markovitz <dumarkov@microsoft.com>
2023-05-08 11:55:16 +0300

    My commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using My.Extensions;
+
+var a = new ClassA();
+
+foreach (var e in a.DoSomething(1))
+{
+    Console.WriteLine(e);
+}
+
+a = null;
+foreach (var e in a.DoSomething(2))
+{
+    Console.WriteLine(e);
+}
+
+foreach (var e in a?.DoSomething(3))
+{
+    Console.WriteLine(e);
+}
+
+public class ClassA {}
+
+namespace My.Extensions
+{
+    public static class ExtClassA
+    {
+        public static IEnumerable<int> DoSomething(this ClassA a, int i)
+        {
+            Console.WriteLine($"DoSomething executed ({i})");
+
+            return Enumerable.Range(1, 3);
+        }
+    }
+}
